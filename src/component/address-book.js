1ca862b6dd42cc23183139dca1d26655e1fe66f1
vcordero07 <vcordero07@gmail.com>
2017-12-02 23:38:06 -0500

    wip

--- src/component/address-book.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import Contact from './contact';
-
-// The AddressBook component goes here.  It should be the default export.
--- src/component/contact.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import './contact.css';
-
-// The Contact component goes here.  It should be the default export.
